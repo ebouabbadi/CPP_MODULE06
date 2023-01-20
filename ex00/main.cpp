1fe5ce6341a35309652b103df104dce3b329dd1f
ebouabba <84751335+ebouabba@users.noreply.github.com>
2023-01-20 12:25:42 +0100

    Add files via upload

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,31 @@
 #include "Convert.hpp"
 
+int ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9' && c == '.')
+		return (1);
+	else
+		return (0);
+}
+
+int parsing(std::string str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 int main(int ac, char **av)
 {
-    std::string str(av[1]);
-    std::cout<<str<<std::endl;
-    return (0);
+	if (ac != 2)
+		return (0);
+	std::string str(av[1]);
+	if (!parsing(str))
+		return (std::cout << "error!!\n", 0);
+	std::cout << str << std::endl;
+	return (0);
 }
