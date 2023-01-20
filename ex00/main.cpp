cae3fd2b4a60ab5f6994afb3e102438100c554b0
El mehdi Bouabbadi <ebouabba@e2r2p2.1337.ma>
2023-01-20 14:58:54 +0100

    <...>

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -11,19 +11,22 @@ int ft_isdigit(int c)
 int parsing(std::string str)
 {
 	int i = 0;
-	int len = str.length();
-	while (str[i])
+	while (str[i] && str.length() != 1)
 	{
-		if (!ft_isdigit(str[i]) && str[len - 1] != 'f')
+		if (!ft_isdigit(str[i]) && str[str.length() - 1] != 'f')
 			return (0);
 		i++;
 	}
 	i = 0;
-	bool signed = false; 
-	while (str[i]){
+	int flag = 0;
+	while (str[i])
+	{
 		if (str[i] == '.')
-
+			flag++;
+		i++;
 	}
+	if (flag > 1)
+		return (0);
 	return (1);
 }
 int main(int ac, char **av)
